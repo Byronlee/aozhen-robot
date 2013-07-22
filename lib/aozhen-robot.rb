c7fe5fedb067b45684c0189dd2ad6845a24c9f13
Jackie Lee <freedomprogramer@gmail.com>
2013-07-22 10:43:18 +0800

    bundle gem a new named zozhen-robot

---
+++ lib/aozhen-robot.rb
@@ -0,0 +1,7 @@
+require "aozhen-robot/version"
+
+module Aozhen
+  module Robot
+    # Your code goes here...
+  end
+end
---
+++ lib/aozhen-robot/version.rb
@@ -0,0 +1,5 @@
+module Aozhen
+  module Robot
+    VERSION = "0.0.1"
+  end
+end
