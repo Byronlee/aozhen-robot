d104d1994f898cb8fdaadc60d54719355cf87ba3
simlegate <simlegate@163.com>
2013-07-24 16:43:19 +0800

    fix #6 package action to command in Turn

--- lib/aozhen-robot/turn.rb
+++ lib/aozhen-robot/turn.rb
@@ -7,15 +7,15 @@ class Turn
    # action left 'L' or right 'R'
    def initialize dir, action
      @dir    = dir
-     @action = action
+     @cmd = Command.new action
    end
 
    def excute
      # refactor
-     if action_L?
+     if @cmd.cmd_L?
        index = left.index(@dir)
        @dir  = left[index - 1]
-     elsif action_R?
+     elsif @cmd.cmd_R?
        index = right.index(@dir)
        @dir  = right[index + 1] || right.first
      end
@@ -29,14 +29,6 @@ class Turn
 
    def right
      @right ||= ['E','S','W','N']
-   end
-   
-   def action_L?
-     @action == 'L'
-   end
-
-   def action_R?
-     @action == 'R'
    end
 end
 
